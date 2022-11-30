6b9de31971faa395defb10a06be5a14a569f9384
Akhlaque <akhlaquea01@gmail.com>
2022-12-01 01:29:26 +0530

    Day 1 : Till passing props to children component

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,27 @@
+let productCount = 0;
+let style = {
+  padding: "0px 20px",
+  fontSize: 12,
+};
+function displayFormattedProductCount() {
+  return productCount > 0 ? productCount : "Zero";
+}
+function ProductDetails(props) {
+  let badgeClass = "badge badge-margin-left-240 ";
+  badgeClass += props.isAvailable === true ? "bg-success" : "bg-danger";
+  return (
+    <div className="d-flex align-items-center justify-content-start mt-1">
+      <h6 className="font-weight-bold my-2" style={{ "margin-right": 30 }}>
+        {props.price}
+      </h6>
+      <button className="btn btn-primary">-</button>
+      <span style={style}>{displayFormattedProductCount()}</span>
+      <button className="btn btn-primary">+</button>
+      <span className={badgeClass}>
+        {props.isAvailable ? "Available" : "Unavailable"}
+      </span>
+    </div>
+  );
+}
+
+export default ProductDetails;
---
+++ src/components/products.js
@@ -0,0 +1,28 @@
+import ProductDetails from "./ProductDetails";
+
+// import { Fragment } from "react";
+// let imageUrl = require("../images/fresh-milk.png");
+
+function Products(props) {
+  return (
+    <li className="list-group-item">
+      <div className="media align-item-lg-center flex-column flex-lg-row p-3 d-flex">
+        <div className="media-body order-2 order-lg-1">
+          <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
+          <p className="font-italic text-muted mb-0 small">
+            {props.description}
+          </p>
+          <ProductDetails price={props.price} isAvailable={props.isAvailable} />
+        </div>
+        <img
+          src={props.imageUrl}
+          width="200"
+          alt="Genric placeholde img"
+          className="ml-lg-5 order-1 order-lg-1"
+        />
+      </div>
+    </li>
+  );
+}
+
+export default Products;
